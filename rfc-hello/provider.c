d01d7f1dd7019832fd4090b31b1cfd7ce3367748
Karol Hrdina <karol.hrdina@gmail.com>
2018-10-05 19:00:15 +0200

    PROBLEM: Corner cases not checked

--- rfc-hello/provider.c
+++ rfc-hello/provider.c
@@ -10,9 +10,13 @@ int main (int argc, char **argv)
         exit(0);
     }
     zsock_t *rep = zsock_new_rep (argv[1]);
+    assert (rep);
 
     while (!zsys_interrupted) {
         char *string = zstr_recv(rep);
+        if (!string)
+            break;
+
         if (strcmp(string,"HELLO") == 0) {
             zstr_send(rep, "OK");
         } else {
