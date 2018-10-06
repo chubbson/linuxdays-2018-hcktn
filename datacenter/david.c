d28f3b2c02a209420b0082f6f13bf157f6401eab
david hauri <chubbso@gmail.com>
2018-10-06 17:49:11 +0200

    setup client with defined endpoint

--- datacenter/david.c
+++ datacenter/david.c
@@ -1,7 +1,21 @@
 #include <malamute.h>
 
+// build: gcc -lmlm -lczmq -g  mvy-pubc -o mvy-pub 
 //20181006 - david hauri
+
+// serverpart by Karol
+static const char *ENDPOINT = "tcp://192.168.1.164:9999"
+static const char *STREAM = "upses";
+
+
+
 int main () {
+    mlm_client_t *client = mlm_client_new();
+    assert(client);
+
+    int rv = mlm_client_connect(client, ENDPOINT, 2000, "david");
+    assert(rv >= 0);
+
    // do nothing special 
 }
 
