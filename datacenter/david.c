7138e6de9cbe2f0ea49128083223675a5199bf7c
david hauri <chubbson@gmail.com>
2018-10-07 08:58:08 +0200

    fixing builderror and return EXIT_SUCCESS

--- datacenter/david.c
+++ datacenter/david.c
@@ -6,20 +6,21 @@
 // serverpart by Karol
 static const char *ENDPOINT = "tcp://192.168.1.164:9999";
 static const char *STREAM = "upses";
+static const char *CLIENTIDENTIFICATION = "david";
 
 static const char *SIGNAL_ON = "ON";
 static const char *SIGNAL_OFF = "OFF";
 
 int randSeconds1to5()
 {
-  return randof(1000) * 1 + 1
+  return randof(5) * 1 + 1;
 }
 
 int main () {
     mlm_client_t *client = mlm_client_new();
     assert(client);
 
-    int rv = mlm_client_connect(client, ENDPOINT, 2000, "david");
+    int rv = mlm_client_connect(client, ENDPOINT, 2000, CLIENTIDENTIFICATION);
     assert(rv >= 0);
 
     srandom ((unsigned) time (NULL));
@@ -27,9 +28,11 @@ int main () {
     while(!zsys_interrupted)
     {
       
-
+      
     }
 
     mlm_client_destroy(&client);
+
+    return EXIT_SUCCESS;
 }
 
